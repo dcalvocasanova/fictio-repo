ecf178097cc000d2dddb04acc3e4615e54db6fcf
dcalvocasanova <dcalvocasanova@gmail.com>
2019-05-27 10:57:17 -0600

    Inicio del proyecto, primer propuesta interfaz gráfica y funcionalidades de autentiación y control de usuarios

---
+++ README.md
@@ -0,0 +1,7 @@
+## Instalación
+* Instalar npm  https://www.npmjs.com/get-npm
+* Renombrar el `.env.example` a `.env` y colocar el string de conexión
+* Generar una contraseña del app usando el comando ` php artisan key:generate`
+* Ejecutar el comando ` npm install ` para instalar las dependencias necesarias
+* Ejecutar los migrates y la ejecución del server
+* Finalmente usar el comando ` npm run watch ` cuando se hacen cambios en la UI  
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+        //Create a Policies for user roles
+        Gate::define('isAdmin',function($user){
+            return $user->type === 'admin';
+        });
+
+        Gate::define('isUser',function($user){
+            return $user->type === 'user';
+        });
+
+        Passport::routes();
+    }
+}
